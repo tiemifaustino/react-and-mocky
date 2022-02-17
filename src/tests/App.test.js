db2395d44febd48fb10ddf665b36d4ea141c8c75
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-17 17:10:43 -0300

    Teste 1 feito.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import responseAPI from './mocks'
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
     //Fazer o mock do fetch aqui 
-    
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(responseAPI),
+    })
     render(<App/>)
   })
   
-  test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
-
+  test('Verifica se aparece o card com titulo de "Rick Sanchez"', async () => {
+    const rickEl = await screen.findByRole('heading', {level: 3, name: /Rick Sanchez/i});
+    expect(rickEl).toBeInTheDocument();
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
