c20537cdf0ef0f3c460e2c885c63d57b6d5d2566
matheus-linux <matheusjcx@gmail.com>
2022-02-17 17:14:31 -0300

    primeiro commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import  responseAPI  from './mocks'
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
-    //Fazer o mock do fetch aqui 
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(responseAPI)
+    })
     
     render(<App/>)
   })
+
   
   test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
-
+    const h3El = screen.getAllByRole('heading', {level: 3});
+    const phraseEl = screen.getByText('Rick Sanchez');
+    expect(h3El && phraseEl).toBeInTheDocument();
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
